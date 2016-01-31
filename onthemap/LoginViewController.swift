52870c7ed1ce04ea3bf24655055c3390c36f8c1b
Sae86 <Sae@saes-macbook-pro.local>
2016-01-30 21:10:15 -0600

    Added Table and map views with tab controller

--- onthemap/LoginViewController.swift
+++ onthemap/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  onthemap
 //
 //  Created by Sae on 1/28/16.
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
---
+++ onthemap/MapViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MapViewController.swift
+//  onthemap
+//
+//  Created by Sae on 1/30/16.
+//  Copyright © 2016 Sae. All rights reserved.
+//
+
+import Foundation
